a0ad8fff415e9c6884563442e44e2bc9f7744aa6
ZemooGames <58574930+ZemooGames@users.noreply.github.com>
2019-12-08 00:15:59 -0500

    Add files via upload

---
+++ Destination/Assets/Test/TestObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestObject : PoolObject {
+
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localScale += Vector3.one * Time.deltaTime * 3;
+        transform.Translate(Vector3.up * Time.deltaTime * 25);
+	}
+
+    public override void OnObjectReuse()
+    {
+        transform.localScale = Vector3.one;
+    }
+}
